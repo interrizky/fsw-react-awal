15e2835de2edd5578f93c2417f2b2c46d16316bb
Rizky Ridho <interrizky@ymail.com>
2021-11-06 22:22:21 +0700

    make hover effect

--- src/Components/ListProductx.js
+++ src/Components/ListProductx.js
@@ -8,25 +8,17 @@ let dataImage = ["Bunga 1", "Bunga 2", "Bunga 3", "Bunga 4", "Bunga 5", "Bunga 6
 
 class ListProductx extends React.Component {
   HoverImage = (event) => {
-    console.log(event)
-    console.log(event.target)
-    console.log(event.relatedTarget)
-
     /* dsp foreground */
-    // event.target.style += 'background-color: grey; opacity: 30%;'
+    document.querySelector(`.dsp-foreground-${event.target.id}`).style.cssText += 'background-color: grey; opacity: 30%;'
     /* dsp background */
-    // event.relatedTarget.style = 'position: relative; top: 150px'
+    document.querySelector(`.dsp-background-${event.target.id}`).style.cssText = 'position: relative; top: 125px'
   }
 
-  HoverOutImage = (e) => {
-    console.log(e)
-    console.log(e.target)
-    console.log(e.relatedTarget)
-
-    /* dsp foreground */
-    // e.target.style = 'unset'
-    /* dsp background */
-    // e.relatedTarget.style = 'unset'
+  HoverOutImage = (events) => {
+    /* foreground */
+    document.querySelector(`.dsp-foreground-${events.target.id}`).style.cssText = 'position: absolute;'
+    /* background */
+    document.querySelector(`.dsp-background-${events.target.id}`).style.cssText = 'position: relative; top: 125px; display:none'
   }
 
   render(){
@@ -41,7 +33,6 @@ class ListProductx extends React.Component {
         }}>
           {
             // How To Looping, but with array data
-            // taruh event di kotakWrapper failed 
             dataImage.map((data, index) => {
                 return(
                   <Col md="4" key={ index+1 } className={ `kotakWrapper wrapper-product-${index+1} shadow-lg rounded` } style={{ 
@@ -52,16 +43,16 @@ class ListProductx extends React.Component {
                     "textAlign": "center",
                     "flexDirection": "column",
                     "width": "350px",
-                    "height": "350px",
+                    "height": "300px",
                     "margin": "25px",                    
                   }}>
-                    <Col className={ `display-depan dsp-foreground-${index+1}` } id={ `dsp-foreground-${index+1}` } style={{ "position": "absolute" }} onMouseLeave={ this.HoverOutImage } onMouseOver={ this.HoverImage } >
-                      <img src={ require(`../Assets/images/pngfind-${index+1}-transparent.png`) } alt={data} style={{ "width": "200px", "height": "175px", "marginTop": "25px" }}  ></img>
-                      <p className="text-wrap" style={{"marginTop": "16px", "width": "200px"}}>{data}</p>
+                    <Col style={{ "position": "absolute" }} className={ `display-depan dsp-foreground-${index+1}` } id={ `${index+1}` } onMouseOver={ this.HoverImage } onMouseLeave={ this.HoverOutImage } >
+                      <img id={ `${index+1}` } src={ require(`../Assets/images/pngfind-${index+1}-transparent.png`) } alt={data} style={{ "width": "200px", "height": "175px", "marginTop": "25px" }}  ></img>
+                      <p id={ `${index+1}` } className="text-wrap" style={{"marginTop": "16px", "width": "200px"}}>{data}</p>
                     </Col>
-                    <Col className={ `dsp-background-${index+1}` } id={ `dsp-background-${index+1}` } style={{ "position": "relative", "top": "150px", "display": "none" }}>
+                    <Col className={ `dsp-background-${index+1}` } id={ `dsp-background-${index+1}` } style={{ "position": "relative", "top": "125px", "display": "none" }}>
                       <a href="https://google.com">
-                        <Button className={ `tombol-info-${index+1}` } style={{ "width": "150px", "height": "50px" }} variant='dark'>Href</Button>
+                        <Button className={ `tombol-info-${index+1}` } style={{ "width": "175px", "height": "50px" }} variant='dark'>Href</Button>
                       </a>
                     </Col>                                    
                   </Col>                  
